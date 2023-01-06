19b54650d41fe50ed7e4af6dfcf296109202e6a4
cazzt <maestre-2001@hotmail.com>
2023-01-06 17:30:35 -0500

    add: transition

--- app.js
+++ app.js
@@ -12,11 +12,27 @@ function PageTransitions(){
             currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
             this.className += 'active-btn';
         })
-    }
+    };
 
     //section Active class
-    allSections.addEventListener('click', (e) =>{
-        
+    allSections[0].addEventListener('click',(e)=>{
+        console.log(e.target);
+        const id= e.target.dataset.id;
+        if(id){
+            //remove selected from the others btns
+            secBtns.forEach((btn) =>{
+                btn.classList.remove('active')
+            })
+            e.target.classList.add('active')
+
+            //hide other sections
+            sections.forEach((section)=>{
+                section.classList.remove('active')
+            })
+
+            const element = document.getElementById(id);
+            element.classList.add('active')
+        }
     })
 }
 
